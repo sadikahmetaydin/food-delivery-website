8f6132951d0c5308df509183e66c1c3a830078b0
sadikahmetaydin <sadikahmetaydin@gmail.com>
2024-07-04 21:35:35 +0300

    Food Delivery Website Project Final

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,6 +6,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = 'http://localhost:4000';
+  const [token, setToken] = useState('');
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -39,6 +41,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken,
   };
 
   return (
